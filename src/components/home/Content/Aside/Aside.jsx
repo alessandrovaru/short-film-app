e1b74763db7ca11f8f423b9436a660f87078cc36
Alessandro <alessandrovaru@gmail.com>
2024-05-26 13:50:11 -0400

    chore: Update CSS for Aside and MainContent components

--- src/components/home/Content/Aside/Aside.jsx
+++ src/components/home/Content/Aside/Aside.jsx
@@ -1,8 +1,34 @@
+import Image from 'next/image';
 import styles from './Aside.module.css';
 export const Aside = (props) => {
   return (
     <div className={styles.container}>
-      <h1>Aside</h1>
+      <div className={styles.imageContainer}>
+        <Image
+          src="/images/logoCannes.jpg"
+          alt="Cannes logo"
+          fill
+          loading='eager'
+        />
+      </div>
+      <div className={styles.imageContainer}>
+        <Image
+          src="/images/logoBifa.jpg"
+          alt="Cannes logo"
+          fill
+          loading='eager'
+        />
+      </div>
+      <ul>
+        <li><strong>Genre:</strong> Drama</li>
+        <li><strong>Running Time:</strong> 15’</li>
+        <li><strong>Aspect Ratio:</strong> 1:2.39</li>
+        <li><strong>Screening Formats:</strong> 2K 5.1 DCP, 35 mm print (French subs), ProRes.</li>
+        <li><strong>Sound:</strong> 5.1 or Stereo.</li>
+        <li><strong>Shot on:</strong> Arri Alexa, 24p, original format 2.8K</li>
+        <li><strong>Language:</strong> Ukrainian, English.</li>
+        <li><strong>Subtitles:</strong> English, Italian, French, Spanish, German, Hebrew, Chinese, Russian, Korean, Arabic.</li>
+      </ul>
     </div>
   );
 }
