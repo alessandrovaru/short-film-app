50b4caafe12fda8416cd5295b1f48fc82722ca34
Alessandro <alessandrovaru@gmail.com>
2024-05-26 12:26:16 -0400

    chore: Refactor page.js to use modular components

---
+++ src/components/home/Content/Content.jsx
@@ -0,0 +1,9 @@
+import styles from "./Content.module.css";
+
+export const Content = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Home Content</h1>
+    </div>
+  );
+}
---
+++ src/components/home/Hero/Hero.jsx
@@ -0,0 +1,8 @@
+import styles from './Hero.module.css';
+export const Hero = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Hero</h1>
+    </div>
+  );
+}
