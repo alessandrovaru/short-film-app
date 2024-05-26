ae2b8af8e44859487edbe5a2bd910de318a0c821
Alessandro <alessandrovaru@gmail.com>
2024-05-26 13:25:32 -0400

    chore: Update CSS for Aside and MainContent components

--- src/components/home/Content/mainContent/MainContent.jsx
+++ src/components/home/Content/mainContent/MainContent.jsx
@@ -2,7 +2,22 @@ import styles from './MainContent.module.css';
 export const MainContent = (props) => {
   return (
     <div className={styles.container}>
-      <h1>MainContent</h1>
+      <h2>Logline</h2>
+      <p>lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <hr />
+      <h2>Synopsis</h2>
+      <p>lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <hr/>
     </div>
   );
 }
