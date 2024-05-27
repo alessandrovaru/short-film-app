c44a19f3db3dc68a7d0c8398a5110feec8acb6a3
Alessandro <alessandrovaru@gmail.com>
2024-05-26 23:06:21 -0400

    Refactor image loading in InlineCrew component

--- src/components/home/Crew/InlineCrew/InlineCrew.jsx
+++ src/components/home/Crew/InlineCrew/InlineCrew.jsx
@@ -1,48 +1,41 @@
 import Image from "next/image";
 import styles from "./InlineCrew.module.css";
 
+const data = [
+  {
+    image: 'https://picsum.photos/600/300',
+    title: 'Verónica Guillén - Directora',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec eros ut erat facilisis pharetra. Fusce nec felis non felis sollicitudin fermentum',
+    imageAlign: 'left'
+  },
+  {
+    image: 'https://picsum.photos/600/300',
+    title: 'Verónica Guillén - Directora',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec eros ut erat facilisis pharetra. Fusce nec felis non felis sollicitudin fermentum',
+    imageAlign: 'right'
+  },
+  {
+    image: 'https://picsum.photos/600/300',
+    title: 'Verónica Guillén - Directora',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec eros ut erat facilisis pharetra. Fusce nec felis non felis sollicitudin fermentum',
+    imageAlign: 'left'
+  },
+];
+
 export const InlineCrew = () => {
   return (
     <div className={`${styles.container} container`}>
-      <div className={styles.inlineFlex}>
-        <div className={styles.imageContainer}>
-          <Image src={'https://picsum.photos/600/300'} alt="crew" fill loading="lazy" />
+      {data.map((item, index) => (
+        <div key={index} className={item.imageAlign === 'left' ? styles.inlineFlex : styles.inlineFlex2}>
+          <div className={item.imageAlign === 'left' ? styles.imageContainer : styles.imageContainer2 }>
+            <Image src={item.image} alt="crew" fill loading="lazy" />
+          </div>
+          <div className={styles.contentContainer}>
+            <h3 className={styles.title}>{item.title}</h3>
+            <p className={styles.text}>{item.text}</p>
+          </div>
         </div>
-        <div className={styles.contentContainer}>
-          <h3 className={styles.title}>Verónica Guillén - Directora</h3>
-          <p className={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            nec eros ut erat facilisis pharetra. Fusce nec felis non felis
-            sollicitudin fermentum
-          </p>
-        </div>
-      </div>
-      <div className={styles.inlineFlex2}>
-        <div className={styles.contentContainer}>
-          <h3 className={styles.title}>Verónica Guillén - Directora</h3>
-          <p className={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            nec eros ut erat facilisis pharetra. Fusce nec felis non felis
-            sollicitudin fermentum
-          </p>
-        </div>
-        <div className={styles.imageContainer2}>
-          <Image src={'https://picsum.photos/600/300'} alt="crew" fill loading="lazy" />
-        </div>
-      </div>
-      <div className={styles.inlineFlex}>
-        <div className={styles.imageContainer}>
-          <Image src={'https://picsum.photos/600/300'} alt="crew" fill loading="lazy" />
-        </div>
-        <div className={styles.contentContainer}>
-          <h3 className={styles.title}>Verónica Guillén - Directora</h3>
-          <p className={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            nec eros ut erat facilisis pharetra. Fusce nec felis non felis
-            sollicitudin fermentum
-          </p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
