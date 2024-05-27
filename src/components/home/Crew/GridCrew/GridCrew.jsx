82016f1631225796e25fe32a81c9e795f6a83a6e
Alessandro <alessandrovaru@gmail.com>
2024-05-26 23:35:04 -0400

    Refactor image loading in GridCrew component

--- src/components/home/Crew/GridCrew/GridCrew.jsx
+++ src/components/home/Crew/GridCrew/GridCrew.jsx
@@ -1,7 +1,54 @@
+import styles from "./GridCrew.module.css";
+import Image from "next/image";
+
+const data = [
+  {
+    name: "John Doe",
+    position: "1st Assistant Director",
+    image: 'https://picsum.photos/300/300',
+  },
+  {
+    name: "Jane Doe",
+    position: "2nd Assistant Director",
+    image: 'https://picsum.photos/300/300',
+  },
+  {
+    name: "John Doe",
+    position: "1st Assistant Director",
+    image: 'https://picsum.photos/300/300',
+  },
+  {
+    name: "Jane Doe",
+    position: "2nd Assistant Director",
+    image: 'https://picsum.photos/300/300',
+  },
+  {
+    name: "John Doe",
+    position: "1st Assistant Director",
+    image: 'https://picsum.photos/300/300',
+  },
+  {
+    name: "Jane Doe",
+    position: "2nd Assistant Director",
+    image: 'https://picsum.photos/300/300',
+  },
+];
+
 export const GridCrew = () => {
   return (
-    <div>
-      <h1>Grid Crew</h1>
+    <div className={`${styles.container} container`}>
+      <h2 className={styles.title}>Our Crew</h2>
+      {data.map((item, index) => (
+        <div key={index} className={styles.card}>
+          <div className={styles.imageContainer}>
+            <Image src={item.image} alt="crew" fill loading="lazy" />
+          </div>
+          <div className={styles.contentContainer}>
+            <h4 className={styles.title}>{item.name}</h4>
+            <p className={styles.text}>{item.position}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
