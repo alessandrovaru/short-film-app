7e7c2113c09a811076634e1ca5636c6f3aae4860
Alessandro <alessandrovaru@gmail.com>
2024-05-26 23:53:47 -0400

    chore: Refactor image loading in Hero component and update styles

--- src/components/home/Hero/Hero.jsx
+++ src/components/home/Hero/Hero.jsx
@@ -1,8 +1,23 @@
+import Image from 'next/image';
 import styles from './Hero.module.css';
+const data = 
+  {
+    title: "Short Film",
+    image: 'https://picsum.photos/1920/1080',
+    cta: 'Watch Now',
+  };
+
+
 export const Hero = () => {
   return (
     <div className={styles.container}>
-      <h1>Hero</h1>
+      <div className={styles.content}>
+        <h1 className={styles.title}>{data.title}</h1>
+        <button className={styles.cta}>{data.cta}</button>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image className={styles.image} src={data.image} alt={data.title} fill />
+      </div>
     </div>
   );
 }
