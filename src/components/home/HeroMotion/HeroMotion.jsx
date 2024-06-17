849edd33f8efdc2eb97a9b69aa5a04bc76abd049
Alessandro <alessandrovaru@gmail.com>
2024-06-17 19:42:25 -0400

    refactor: Improve HeroMotion component styles and structure

--- src/components/home/HeroMotion/HeroMotion.jsx
+++ src/components/home/HeroMotion/HeroMotion.jsx
@@ -17,8 +17,8 @@ export const HeroMotion = ({title, subtitle, position}) =>  {
       <motion.div id={position} style={{y: y, opacity:opacity }}>
         {title ? <h2>{title}</h2> : <></>}
         {subtitle ? <p>{subtitle}</p> : <></>}
-        {position === 1 ? <a className={'btn btn-warning'} href='#2' >▼</a> : <></>}
-        {position === 2 ? <a className={'btn btn-warning'} href='#mainTitle' >▼</a> : <></>}
+        {position === 1 ? <a className={`btn ${styles.btn}`} href='#2' >▼</a> : <></>}
+        {position === 2 ? <a className={`btn ${styles.btn}`} href='#mainTitle' >▼</a> : <></>}
       </motion.div>
     </div>
   )
