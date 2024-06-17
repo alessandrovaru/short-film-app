ac775eb7ca0002f5c442b9a75c5186ccc0da1128
Alessandro <alessandrovaru@gmail.com>
2024-06-17 13:04:37 -0400

    Update background colors and styles for MainMotion component

--- src/components/home/MainMotion/MainMotion.jsx
+++ src/components/home/MainMotion/MainMotion.jsx
@@ -17,7 +17,7 @@ export const MainMotion = ({text}) =>  {
     offset:['start end', 'end start']
   });
 
-  const x = useTransform(scrollYProgress, [0, 0.25, 0.5, 0.75, 1], [ "0vw", "0vw", "-200vw", "-300vw", "-400vw"]);
+  const x = useTransform(scrollYProgress, [0, 0.25, 0.5, 0.75, 1], [ "0vw", "0vw", "-50vw", "-100vw", "-100vw"]);
 
   return (
     <section ref={container} className={`${styles.section} `}>
@@ -26,16 +26,12 @@ export const MainMotion = ({text}) =>  {
             className={styles.textDiv}
           >
             <div className={styles.thumbnail}>
-              <p>Imagina a una mujer sofocada</p>
-            </div>
-            <div className={styles.thumbnail}>
+              <h3>Imagina a una mujer sofocada</h3>
               <p>Elena, atrapada entre el peso de las expectativas y el anhelo desesperado de libertad. Su mundo se reduce al zumbido de monotonía luego de haber perdido a su madre y su trabajo en pocos meses.</p>
             </div>
             <div className={styles.thumbnail}>
-              <p>Pero las montañas susurran</p>
-            </div>
-            <div className={styles.thumbnail}>
-              <p>Siempre le han susurrado a Elena, una presencia constante contra el acero sofocante de la ciudad. Ahora, la invitan con una promesa: una oportunidad para deshacerse de la carga de quién "debería" ser y abrazar a la mujer que desea ser.</p>
+              <h3>Pero las montañas susurran</h3>
+              <p>Siempre le han susurrado a Elena, una presencia constante contra el acero sofocante de la ciudad. Ahora, la invitan con una promesa: una oportunidad para deshacerse de la carga de quién &quot;debería&quot; ser y abrazar a la mujer que desea ser.</p>
             </div>
           </motion.div>
     </section>
