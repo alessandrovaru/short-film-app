2d68e7c9a1952a01f003a5a0e9924049232c423b
Alessandro <alessandrovaru@gmail.com>
2024-06-18 15:47:26 -0400

    refactor: Add lazy loading for MainBanner component image

--- src/components/home/MainBanner/MainBanner.jsx
+++ src/components/home/MainBanner/MainBanner.jsx
@@ -5,7 +5,9 @@ export const MainBanner = (props) => {
   return (
     <div className={`${styles.section}`}>
       <div className={`${styles.container} container`}>
-        <Image key={'main'} src={'/images/portada.png'} alt="Main Banner" fill loading='lazy' />
+        <div className={`${styles.imgcontainer}`}>
+          <Image key={'main'} src={'/images/portada.png'} alt="Main Banner" fill loading='lazy' />
+        </div>
       </div>
     </div>
   );
