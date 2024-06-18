29cff4c59b91658fc9e6a5f5e9a220fa26a9af83
Alessandro <alessandrovaru@gmail.com>
2024-06-18 15:28:31 -0400

    refactor: Update MainTitle component with dynamic data

--- src/components/home/MainTitle/MainTitle.jsx
+++ src/components/home/MainTitle/MainTitle.jsx
@@ -1,10 +1,16 @@
 import styles from './MainTitle.module.css';
+
+const data = {
+  title: "Catársis",
+  header: "Un cortometraje escrito y dirigido por Verónica Guillén",
+  cta: 'Ver más',
+};
 export const MainTitle = () => {
   return (
     <section id='mainTitle' className={`${styles.section} container`}>
-      <p>Un cortometraje escrito y dirigido por Verónica Guillén</p>
-      <h1>Catársis</h1>
-      <a  href="#mainMotion" className={`btn ${styles.btn}`}>Ver más</a>
+      <p>{data.header}</p>
+      <h2>{data.title}</h2>
+      <a href='#mainMotion' className={`btn ${styles.btn}`}>{data.cta}</a>
     </section>
   );
 }
