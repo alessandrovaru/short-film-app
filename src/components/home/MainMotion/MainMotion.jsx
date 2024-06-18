8e7d67bccdd37c0951bce1234e084982f5355ae4
Alessandro <alessandrovaru@gmail.com>
2024-06-17 20:15:44 -0400

    refactor: Improve HeroMotion component styles and structure

--- src/components/home/MainMotion/MainMotion.jsx
+++ src/components/home/MainMotion/MainMotion.jsx
@@ -18,18 +18,25 @@ export const MainMotion = ({text}) =>  {
   });
 
   const x = useTransform(scrollYProgress, [0, 0.25, 0.5, 0.75, 1], [ "0vw", "0vw", "-50vw", "-100vw", "-100vw"]);
+  const scroll100vh = () => {
+    window.scroll({
+      top: window.innerHeight * 7,
+      behavior: 'smooth'
+    });
+  }
 
   return (
-    <section ref={container} className={`${styles.section} `}>
+    <section id="mainMotion" ref={container} className={`${styles.section} `}>
           <motion.div
             style={{ x }}
             className={styles.textDiv}
           >
-            <div className={styles.thumbnail}>
+            <div id="firstPage" className={styles.thumbnail}>
               <h3>Imagina a una mujer sofocada</h3>
               <p>Elena, atrapada entre el peso de las expectativas y el anhelo desesperado de libertad. Su mundo se reduce al zumbido de monotonía luego de haber perdido a su madre y su trabajo en pocos meses.</p>
+              <a onClick={scroll100vh} className={`btn ${styles.btn}`}>→</a>
             </div>
-            <div className={styles.thumbnail}>
+            <div id="secondPage" className={styles.thumbnail}>
               <h3>Pero las montañas susurran</h3>
               <p>Siempre le han susurrado a Elena, una presencia constante contra el acero sofocante de la ciudad. Ahora, la invitan con una promesa: una oportunidad para deshacerse de la carga de quién &quot;debería&quot; ser y abrazar a la mujer que desea ser.</p>
             </div>
--- src/components/home/MainTitle/MainTitle.jsx
+++ src/components/home/MainTitle/MainTitle.jsx
@@ -2,8 +2,9 @@ import styles from './MainTitle.module.css';
 export const MainTitle = () => {
   return (
     <section id='mainTitle' className={`${styles.section} container`}>
-      <p>Escrito y Dirigido por Verónica Guillén</p>
+      <p>Un cortometraje escrito y dirigido por Verónica Guillén</p>
       <h1>Catársis</h1>
+      <a  href="#mainMotion" className={`btn ${styles.btn}`}>Ver más</a>
     </section>
   );
 }
