3df0a9bc3268ad05693db616daef9c0109654043
Alessandro <alessandrovaru@gmail.com>
2024-08-04 12:17:36 -0400

    refactor: Update metadata and add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,8 +9,27 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 const inter = EB_Garamond({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Catarsis",
-  description: "Un cortometraje escrito y dirigido por Verónica Guillén",
+  title: "Catarsis || Cortometraje",
+  description: "Un cortometraje dirigido y escrito por Verónica Guillén y producido por Adriana Zerpa",
+  generator: "Alessandro Varuzza",
+  applicationName: "Catarsis || Short Film Portfolio",
+  referrer: 'origin-when-cross-origin',
+  keywords: "Catarsis, short film, cortometraje, Verónica Guillén, Adriana Zerpa, Venezuela, Caracas, cine, film, short, film, portfolio,",
+  creator: "Alessandro Varuzza",
+  publisher: "Alessandro Varuzza",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  openGraph: {
+    title: "Catarsis || Cortometraje",
+    description: "Un cortometraje dirigido y escrito por Verónica Guillén y producido por Adriana Zerpa",
+    url: "https://short-film-app.vercel.app/",
+    siteName: "Catarsis",
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 // config your font
@@ -23,6 +42,10 @@ const microgramma = localFont({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+       <link rel="icon" href="/images/favicons/favicon.ico" sizes="any" />
+
+      </head>
       <body className={`${inter.className} ${microgramma.variable}`}>{children}</body>
     </html>
   );
