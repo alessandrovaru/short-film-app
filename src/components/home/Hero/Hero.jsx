e08ffa545c821f276a9d54b00295c8ac81ad9e64
Alessandro <alessandrovaru@gmail.com>
2024-08-08 13:33:54 -0400

    Addin spring and new image

--- src/components/home/Hero/Hero.jsx
+++ src/components/home/Hero/Hero.jsx
@@ -1,9 +1,12 @@
-
+'use client'
 import Image from 'next/image';
 import styles from './Hero.module.css';
 import { register } from 'swiper/element/bundle';
+import { useRef } from 'react';
 
 register();
+
+import { useScroll, useSpring, useTransform, motion } from "framer-motion";
 
 const data = {
   title: "Short Film",
@@ -13,11 +16,23 @@ const data = {
 
 export const Hero = () => {
 
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset:['end start', 'start end']
+  });
+
+  const translateX = useTransform(scrollYProgress, [0, 1], ["-80%", "50%"]);
+  const translateXSpring = useSpring(translateX, { stiffness: 100, damping: 10 });
+
+  const scale = useTransform(scrollYProgress, [0, 1], [0.9, 1.9]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [1, 0.04]);
+  
   return (
-    <section className={styles.container}>
-      <div className={styles.imageContainer}>
+    <section ref={container} className={styles.container}>
+      <motion.div className={styles.imageContainer} style={{ y: translateXSpring, scale, opacity }}>
         <Image className={styles.image} src={data.image} alt={data.title} fill />
-      </div>
+      </motion.div>
     </section>
   );
 }
