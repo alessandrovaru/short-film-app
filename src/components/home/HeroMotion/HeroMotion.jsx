d791a1e94d93a9a5a9ad2afe1adf4eaa31ee2b48
Alessandro <alessandrovaru@gmail.com>
2024-08-11 13:24:52 -0400

    Not using framer motion

--- src/components/home/HeroMotion/HeroMotion.jsx
+++ src/components/home/HeroMotion/HeroMotion.jsx
@@ -4,18 +4,13 @@ import { useRef } from "react";
 import styles from './HeroMotion.module.css';
 
 export const HeroMotion = ({title, subtitle, position}) =>  {
-  const container = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: container,
-    offset:['end start', 'start end']
-  });
 
-  const opacity = useTransform(scrollYProgress, [0, 0.25, 0.49, 0.5, 0.51, 0.75, 1], [0, 0, 0.99, 1, 0.99, 0, 0]);
-  const y = useTransform(scrollYProgress, [0, 0.5, 1], [400, 0, -400]);
+
+ 
 
   return (
-    <section ref={container} className={`${styles.textSection}`}>
-      <motion.div className={`${styles.textContainer}`} id={position} style={{y: y, opacity:opacity }}>
+    <section  className={`${styles.textSection} appear`}>
+      <motion.div className={`${styles.textContainer}`} id={position} >
         <>
           {title ? <h2>{title}</h2> : <></>}
           {subtitle ? <p>{subtitle}</p> : <></>}
